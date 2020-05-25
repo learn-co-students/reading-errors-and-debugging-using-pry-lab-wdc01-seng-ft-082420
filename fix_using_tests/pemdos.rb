c213bced5fc3822642a229661da5156d37c9d691
Giulia Cajati <giuliacajati@gmail.com>
2020-05-25 14:11:14 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,8 @@
-# don't forget to add: require 'pry'
+#require 'pry' 
 
 def snake_it_up(string)
-  if string[0] == "s"
-    10 * "s" + string
-  else
-    string
-  end
+  if string[0] == s
+    s_ten = {return 10 * "s"}
+    's_ten#{string}'
+end
 end
