fdec194b752e2ff0d9923ce94cbeac129279e07a
Giulia Cajati <giuliacajati@gmail.com>
2020-05-25 15:19:23 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,7 @@
-#require 'pry' 
-
 def snake_it_up(string)
   if string[0] == 's'
     "ssssssssss" + string
     else
   string
 end
+end 
--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,14 +1,15 @@
 
 
 def generate_star_date
-  (rand(100000) + 400000) / 10.0
+  star_date = (rand(100000) + 400000) / 10.0
+  return star_date 
 end
 
 def state_log(star_date)
- "Captain's Log, star date #{star_date}."
+  "Captain's Log, star date #{star_date}."
 end
 
 def engage
-  puts state_log(date)
   date = generate_star_date
+  puts state_log(date)
 end
